5321e0195e5961a4154ac36969552f7b3a583a92
mehiameen <ameen.kibria@mehilainen.fi>
2025-08-20 12:44:37 +0300

    add encryption key

---
+++ baseline/pingfederate/hooks/00-install-encryption-key.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Copying encryption key from secret mount..."
+cp /opt/staging/instance/server/default/data/pf.jwk /opt/out/instance/server/default/data/pf.jwk 2>/dev/null && echo "✅ Key copied" || echo "❌ Key copy failed"
+EOF
+
+chmod +x pingidentity-server-profiles/baseline/pingfederate/hooks/00-copy-key.sh
